52b104dd0db56965ffea9a31cc2bbca7ff832c16
TheRealKevBot <Kglas001@gmail.com>
2021-04-26 20:18:09 -0600

    Side Bar

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+
+    return (
+        <div className='footer'>
+            <ul className='footer-nav'>
+
+            </ul>
+        </div>
+    )
+}
